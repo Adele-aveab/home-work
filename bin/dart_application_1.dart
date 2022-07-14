1c0f4d780eead5c9b1f60246b3f4398562845070
Adele-aveab <adelya.s.2001@mail.ru>
2022-07-14 20:29:55 +0300

    added new home work files

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -7,7 +7,20 @@ String name = 'My name is ';
 
 print(name.runtimeType);
 
+double b = 4.20;
+int a = 3;
+print(b + a);
 
+
+String text = 'Name';
+text = 'Flutter';
+print('I want to learn $text');
+
+
+double myNumber = 2.225;
+String myText;
+myText = myNumber.toString();
+print(myText);
 
 
 } 
